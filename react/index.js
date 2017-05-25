a53ddf07f07a2ee666c948d8bc0b7d74e094c23b
Neal Fennimore <hi@neal.codes>
2017-05-25 09:36:00 -0700

    Update react config to use latest jsx-a11y rules

--- react/index.js
+++ react/index.js
@@ -13,6 +13,16 @@ module.exports = {
         'react/jsx-indent': [2, 4],
         'react/jsx-uses-vars': 1,
         'react/prop-types': 1,
-        'react/react-in-jsx-scope': 1
+        'react/react-in-jsx-scope': 1,
+
+        'jsx-a11y/alt-text': 2,
+        'jsx-a11y/no-static-element-interactions': 2,
+        'jsx-a11y/no-noninteractive-element-interactions': 2,
+        'jsx-a11y/interactive-supports-focus': 2,
+
+        // Removed in v5
+        'jsx-a11y/img-has-alt': 0,
+        'jsx-a11y/onclick-has-focus': 0,
+        'jsx-a11y/onclick-has-role': 0
     }
 };
