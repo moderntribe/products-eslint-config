6ef878d3d8a9c1f475f49c6f3db456e2c306073b
Neal Fennimore <hi@neal.codes>
2017-05-25 10:30:27 -0700

    Update jsx indents to use tabs

--- react/index.js
+++ react/index.js
@@ -9,8 +9,8 @@ module.exports = {
     ],
     rules: {
         'react/jsx-filename-extension': 0,
-        'react/jsx-indent-props': [2, 4],
-        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 'tab'],
+        'react/jsx-indent': [2, 'tab'],
         'react/jsx-uses-vars': 1,
         'react/prop-types': 1,
         'react/react-in-jsx-scope': 1,
